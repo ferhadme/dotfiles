60159160c55237253b5a5974c04ad3b38d0f00d9
ferhadme <frhdmehdiyev@gmail.com>
2021-09-21 01:53:33 +0400

    .emacs.d

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,4 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
@@ -29,10 +25,6 @@
  '(tool-bar-mode nil))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#1d1f28" :foreground "WhiteSmoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "CTDB" :family "Fira Mono"))))
  '(cursor ((t (:background "white"))))
  '(font-lock-comment-delimiter-face ((t (:foreground "green"))))
