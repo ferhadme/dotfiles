b2ccf071f7623e842215890bf199ebbea7ad96a3
ferhadme <frhdmehdiyev@gmail.com>
2022-02-25 15:24:04 +0400

    .mongoshrc.js welcome msg

--- .mongoshrc.js
+++ .mongoshrc.js
@@ -1,15 +1,16 @@
 function part_of_the_day() {
     let hr = (new Date()).getHours();
-    if (hr >= 0 || hr < 6)
+    if (hr >= 0 && hr < 6)
 	return "night";
-    if (hr >= 6 || hr < 12)
+    if (hr >= 6 && hr < 12)
 	return "morning";
-    if (hr >= 12 || hr < 18)
+    if (hr >= 12 && hr < 18)
 	return "afternoon";
-    if (hr >= 18 || hr < 0)
+    if (hr >= 18 && hr < 0)
 	return "evening";
 }
 
-console.log(`Good ${part_of_the_day} ${process.env.user}!`);
+print(`\nGood ${part_of_the_day()}, ${process.env.USER}!`);
+print('Welcome to the Mongodb!\n')
 
 EDITOR="/usr/bin/vim";
