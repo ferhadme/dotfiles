d31e58556037c1e6cad28202c8997c8bdd5e00f4
ferhadme <frhdmehdiyev@gmail.com>
2022-03-20 16:43:48 +0400

    snippets added

--- .vanilla.emacs.d/init.el
+++ .vanilla.emacs.d/init.el
@@ -172,20 +172,9 @@
 
 ;; Theme from https://github.com/rexim/gruber-darker-theme
 (load-theme 'gruber-darker t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-bullets vi-tilde-fringe use-package treemacs smex shrink-path rich-minority powerline multiple-cursors markdown-mode magit hl-todo gruber-darker-theme company color-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-level-1 ((t (:foreground "#96A6C8" :weight normal))))
- '(org-level-2 ((t (:foreground "#CC8C3C" :weight normal))))
- '(org-level-3 ((t (:foreground "#F4F4FF" :weight normal))))
- '(org-level-4 ((t (:foreground "#F4F4FF" :weight normal))))
- '(org-level-5 ((t (:foreground "#F4F4FF" :weight normal)))))
+
+;; Used snippets is https://github.com/doomemacs/snippets
+(add-to-list 'load-path
+             "~/.emacs.d/snippets")
+(require 'yasnippet)
+(yas-global-mode 1)
